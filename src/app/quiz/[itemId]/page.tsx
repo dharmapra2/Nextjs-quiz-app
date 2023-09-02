61a4c96305fba0f0cbe46a201bef9c33f82a2569
Dharmapra2 <dharmapra865@gmail.com>
2023-09-02 14:06:35 +0530

    update context

--- src/app/quiz/[itemId]/page.tsx
+++ src/app/quiz/[itemId]/page.tsx
@@ -1,27 +1,23 @@
 "use client";
-import React, { useEffect, useMemo, useState } from "react";
-import { responseData } from "@/components/service/Data/category";
+import React, { useContext, useEffect, useMemo, useState } from "react";
 import { singleQuestion } from "@/components/service/Type";
 import { useRouter } from "next/navigation";
-import { RootState } from "@/components/redux/store";
-import { useSelector } from "react-redux";
+import { QuizContext } from "../layout";
 
 
 // we are using memo to prevent unnecessary re render
 function Page({ params }: { params: { itemId: number } }) {
+  const { questionData } = useContext(QuizContext);
+
   const { push } = useRouter();
-  const { questionData }: { questionData: singleQuestion | any | null } = useSelector((state: RootState) => state.question);
 
   const paramsItemId = useMemo(() => +params?.itemId, [params?.itemId]);
-  const noOfQue = useMemo(() => questionData.length, [questionData]);
   const [selectedQue, setSelectedQue] = useState<singleQuestion | null>(null);
   const [options, setOptions] = useState<string[] | null>([]);
   const [active, setActive] = useState<Number>(-1);
 
   useEffect(() => {
-    console.log("paramsItemId:", paramsItemId);
-    console.log("noOfQue:", noOfQue);
-    console.log("disabled:", paramsItemId <= 1);
+    console.log(`context`, questionData);
     const selected = questionData.find(
       ({ itemId }: { itemId: number }) => paramsItemId === itemId
     );
@@ -31,58 +27,62 @@ function Page({ params }: { params: { itemId: number } }) {
   }, [paramsItemId]);
 
   return (
-    <article className="w-full h-full flex flex-col justify-between bg-quiz-valentine-red font-serif">
-      <section className="flex flex-col gap-2 text-base">
-        <div className="max-h-fit flex flex-row items-start text-justify gap-2 p-4 border-4">
-          <strong className="float-left font-bold ">{`Q(${paramsItemId}).`}</strong>
-          <p
-            className="font-semibold pt-[2px]"
-            dangerouslySetInnerHTML={{
-              __html: selectedQue?.question,
-            }}
-          ></p>
-        </div>
-        <div className="h-fit flex flex-col gap-2 p-3 text-justify">
-          <p className="font-normal">
-            Please choose one of the following answers:
-          </p>
-          {/* options */}
-          {options?.length ? (
-            <ul className="flex flex-col gap-1 font-medium">
-              {options?.map((item, index) => (
-                <li
-                  key={index}
-                  className={`flex items-center text-gray-600 dark:text-gray-200 justify-between py-3  border-gray-100 dark:border-gray-800 ${active == index
-                    ? "bg-quiz-flax"
-                    : "bg-transparent border-b-2"
-                    }`}
-                  onClick={() => setActive((_prev) => index)}
-                  dangerouslySetInnerHTML={{
-                    __html: item,
-                  }}
-                ></li>
-              ))}
-            </ul>
-          ) : null}
-        </div>
-      </section>
-      <section className="w-full flex flex-col sm:flex-row justify-around gap-2 items-center text-sm md:text-xl flex-wrap">
-        <button
-          className="flex gap-2 items-center justify-center bg-quiz-mint hover:bg-quiz-mint-15 focus:outline-none focus:ring focus:ring-violet-300 active:bg-quiz-navy px-5 py-2 leading-5 rounded-full font-semibold text-white w-[calc(100%-10px)] sm:w-[40%] h-12 disabled:bg-quiz-grey disabled:text-quiz-grey-15"
-          disabled={paramsItemId <= 1 ? true : false}
-          onClick={() => push(`/quiz/${paramsItemId - 1}`)}
-        >
-          <span className="hidden md:block font-extrabold">{"<"}</span>Prev
-        </button>
-        <button
-          className="flex gap-2 items-center justify-center bg-quiz-pink hover:bg-quiz-pink-15 focus:outline-none focus:ring focus:ring-violet-300 active:bg-quiz-navy px-5 py-2 leading-5 rounded-full font-semibold text-white w-[calc(100%-10px)] sm:w-[40%] h-12 disabled:bg-quiz-grey disabled:text-quiz-grey-15"
-          disabled={paramsItemId >= noOfQue - 1 ? true : false}
-          onClick={() => push(`/quiz/${paramsItemId + 1}`)}
-        >
-          Next<span className="hidden md:block font-extrabold">{">"}</span>
-        </button>
-      </section>
-    </article>
+    <QuizContext.Consumer>
+      {(context: any) => (
+        <article className="w-full h-full flex flex-col justify-between bg-quiz-valentine-red font-serif">
+          <section className="flex flex-col gap-2 text-base">
+            <div className="max-h-fit flex flex-row items-start text-justify gap-2 p-4 border-4">
+              <strong className="float-left font-bold ">{`Q(${paramsItemId}).`}</strong>
+              <p
+                className="font-semibold pt-[2px]"
+                dangerouslySetInnerHTML={{
+                  __html: selectedQue?.question,
+                }}
+              ></p>
+            </div>
+            <div className="h-fit flex flex-col gap-2 p-3 text-justify">
+              <p className="font-normal">
+                Please choose one of the following answers:
+              </p>
+              {/* options */}
+              {options?.length ? (
+                <ul className="flex flex-col gap-1 font-medium">
+                  {options?.map((item, index) => (
+                    <li
+                      key={index}
+                      className={`flex items-center text-gray-600 dark:text-gray-200 justify-between py-3  border-gray-100 dark:border-gray-800 ${active == index
+                        ? "bg-quiz-flax"
+                        : "bg-transparent border-b-2"
+                        }`}
+                      onClick={() => setActive((_prev) => index)}
+                      dangerouslySetInnerHTML={{
+                        __html: item,
+                      }}
+                    ></li>
+                  ))}
+                </ul>
+              ) : null}
+            </div>
+          </section>
+          <section className="w-full flex flex-col sm:flex-row justify-around gap-2 items-center text-sm md:text-xl flex-wrap">
+            <button
+              className="flex gap-2 items-center justify-center bg-quiz-mint hover:bg-quiz-mint-15 focus:outline-none focus:ring focus:ring-violet-300 active:bg-quiz-navy px-5 py-2 leading-5 rounded-full font-semibold text-white w-[calc(100%-10px)] sm:w-[40%] h-12 disabled:bg-quiz-grey disabled:text-quiz-grey-15"
+              disabled={paramsItemId <= 1 ? true : false}
+              onClick={() => push(`/quiz/${paramsItemId - 1}`)}
+            >
+              <span className="hidden md:block font-extrabold">{"<"}</span>Prev
+            </button>
+            <button
+              className="flex gap-2 items-center justify-center bg-quiz-pink hover:bg-quiz-pink-15 focus:outline-none focus:ring focus:ring-violet-300 active:bg-quiz-navy px-5 py-2 leading-5 rounded-full font-semibold text-white w-[calc(100%-10px)] sm:w-[40%] h-12 disabled:bg-quiz-grey disabled:text-quiz-grey-15"
+              disabled={paramsItemId >= context?.noOfQue - 1 ? true : false}
+              onClick={() => push(`/quiz/${paramsItemId + 1}`)}
+            >
+              Next<span className="hidden md:block font-extrabold">{">"}</span>
+            </button>
+          </section>
+        </article>
+      )}
+    </QuizContext.Consumer>
   );
 }
 
--- src/app/quiz/layout.tsx
+++ src/app/quiz/layout.tsx
@@ -1,35 +1,54 @@
 "use client";
 import NavBar from "@/components/widget/NavBar";
 import TopNavigation from "@/components/widget/TopNavigation";
-import React, { createContext, useContext, useState } from "react";
-import { QuizContext } from "../layout";
+import React, { createContext, useContext, useMemo, useState } from "react";
+import { RootState } from "@/components/redux/store";
+import { singleQuestion } from "@/components/service/Type";
+import { useSelector } from "react-redux";
 
 const style = {
   container: `grid grid-cols-6 grid-rows-8 gap-4 bg-green-500`,
   main: `h-screen w-full overflow-y-auto pb-4 mobile-sm:pb-4 pt-4 px-[7px] mobile-sm:px-[12px] tablet-lg:px-[12px] tablet-xl:px-[23px] tablet:px-0 tablet:pb-4 tablet:pt-0 tablet-lg:pt-0`,
   sideList: `col-span-2 row-span-7 row-start-2`,
 };
+export const QuizContext = createContext<{
+  questionData: singleQuestion[] | any[];
+  noOfQue: number;
+  msg: String
+}>({ questionData: [], noOfQue: 0, msg: "" });
 
 function QuizLayout({ children }: { children: React.ReactNode }) {
+  const isClient = typeof window !== "undefined";
+
+  // Initialize questionData and noOfQue with default values
+  let questionData: singleQuestion[] | any[] = [];
+  let noOfQue = 0;
+  let msg = "fghjk";
+
+  // Use useSelector only on the client side
+  if (isClient) {
+    const { questionData: clientQuestionData }: { questionData: singleQuestion[] | any[] } = useSelector((state: RootState) => state.question);
+    questionData = clientQuestionData;
+    noOfQue = useMemo(() => questionData.length, [questionData]);
+  }
+
   return (
-    <QuizContext.Consumer>
-      {(contex: any) => (
-        <main
-          className="flex w-full h-full flex-col justify-between gap-2 overflow-hidden"
-          style={{ fontFamily: "ProximaNova" }}
-        >
-          <NavBar />
-          <div className="flex flex-col md:flex-row gap-2 w-full h-[calc(100%-10%)]">
-            <TopNavigation
-              className={`w-full h-[15%] md:h-full md:max-w-[350px] bg-white overflow-auto p-2`}
-            />
-            <div className="w-full h-full bg-yellow-900 overflow-auto p-2 bg-quiz-plum">
-              {children}
-            </div>
+    <QuizContext.Provider value={{ questionData, noOfQue, msg }}>
+      <main
+        className="flex w-full h-full flex-col justify-between gap-2 overflow-hidden"
+        style={{ fontFamily: "ProximaNova" }}
+      >
+        <NavBar />
+        <div className="flex flex-col md:flex-row gap-2 w-full h-[calc(100%-10%)]">
+          <TopNavigation
+            className={`w-full h-[15%] md:h-full md:max-w-[350px] bg-white overflow-auto p-2`}
+          />
+          <div className="w-full h-full bg-yellow-900 overflow-auto p-2 bg-quiz-plum">
+            {children}
           </div>
-        </main>
-      )}
-    </QuizContext.Consumer>
+        </div>
+      </main>
+    </QuizContext.Provider>
   );
 }
 
